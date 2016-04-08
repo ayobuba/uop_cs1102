da67761617ba36f77b78963abdd76f73cd85aefb
Daniel Strong <danielstrong@mac.com>
2016-04-08 11:00:52 -0800

    add hello world file

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+/**
+ *
+ * @author strongdan
+ */
+public class HelloWorld {
+public static void main(String[] arguments){
+// Program execution begins here
+System.out.println("Hello world.");
+}    
+}
