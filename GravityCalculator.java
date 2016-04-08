4739edb44cd0767136cdefaca058a6fd6820487c
Dan Strong <strongdan@users.noreply.github.com>
2016-04-07 16:56:10 -0800

    Create GravityCalculator.java

---
+++ GravityCalculator.java
@@ -0,0 +1,21 @@
+ 	import java.io.*;
+
+public class GravityCalculator {
+
+	public static void main(String[] arguments){
+
+		double  gravity =-9.81; // Earth's gravity in m/s^2
+
+		double  initialVelocity = 0.0; // starting velocity of the object
+
+		double  fallingTime = 10.0; // time in seconds that the object falls
+
+		double  initialPosition = 1000.0; // Starting position in meters, the calculation will 		determine the ending position in meters
+
+		double  finalPosition = 0.0;
+
+		System.out.println("The object's position after " + fallingTime +" seconds is 			"+finalPosition + " m.");
+
+	}
+}
+  
