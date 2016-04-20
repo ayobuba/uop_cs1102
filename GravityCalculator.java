80fed005a620527b194b649025caaf9f73ae95df
Daniel Strong <danielstrong@mac.com>
2016-04-19 20:28:54 -0800

    various changes

--- GravityCalculator.java
+++ GravityCalculator.java
@@ -17,19 +17,19 @@ public class GravityCalculator {
      * @param args the command line arguments
      */
     public static void main(String[] arguments) {
+
         double  gravity =-9.81; // Earth's gravity in m/s^2
 
-		double  initialVelocity = 0.0; // starting velocity of the object
+    		double  initialVelocity = 0.0; // starting velocity of the object
 
-		double  fallingTime = 10.0; // time in seconds that the object falls
+    		double  fallingTime = 10.0; // time in seconds that the object falls
 
-		double  initialPosition = 1000.0; // Starting position in meters, the calculation will 		determine the ending position in meters
-                    
-		double  finalPosition = 0.5 * ((gravity) * (Math.pow(fallingTime, 2))) + initialVelocity + initialPosition;
-                /* x(t) = 0.5at^2 + vit + xi --> indicates the position in meters of an object after falling for 10 seconds in Earths gravity */
-		System.out.println("The object's position after " + fallingTime +" seconds is 			"+finalPosition + " m.");
+    		double  initialPosition = 1000.0; // Starting position in meters, the calculation will 		determine the ending position in meters
+                        
+    		/* finalPosition = (gravity * (fallingTime * fallingTime)/2) + (initialVelocity * fallingTime) + initialPosition; */
+                /* x(t) = 0.5at^2 + vit + xi --> indicates the position in meters of an object after falling for 10 seconds 
+                in Earths gravity */
+    		System.out.println("The object's position after " + fallingTime +" seconds is "+ finalPosition + " m.");
 
 	}
 }
-
-
