007f6fbe39e22f3409891108633c8c3e2dbf68b0
Daniel Strong <danielstrong@mac.com>
2016-05-03 16:38:15 -0800

    add first subroutine files

--- firstsubroutines/src/firstsubroutines/Firstsubroutines.java
+++ firstsubroutines/src/firstsubroutines/Firstsubroutines.java
@@ -1,57 +1,67 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package firstsubroutines;
 
-/**
- *
- * @author brocd8s
- */
-public class Main () {
-        // print the string converted to lower case and stripped of any non-letter characters
-        // Print the reverse string and say whether the string is a palindrome
-        public static void main(String[] args)
-        // prompt the user to enter a string
-        String text = ""; //
-        String stripped = ""; //
-        String reversed = ""; //
-        System.out.println("Enter a string to find out if it's a palindrome ");
-        text = user_input.next();
-        // test the string entered by the user to determine whether it is a palindrome
-        if strReverse(text) == text:
-            System.out.println("This IS a palindrome.");
-        else:
-            System.out.println("This is NOT a palindrome.");
-        // convert the string to lower case
-        // remove any non-letter characters from the string
-        // compare the resulting string with the reverse of the same string
-        // If strings are equal, then the original string is considered to be a palindrome
+public class Firstsubroutines {
+   // Subroutines stripString and revString
     
-    System.out.println(stripped);
-    // finds the reverse of a string
-    static String strReverse (String str) {
- 	String reverse;
-  	int i;
-  	reverse = "";
-  	for (i = str.length() - 1; i >= 0; i--) {
-   		reverse = reverse + str.charAt(i);      
-        return reverse;
-  }
-    // make a new string that is a copy of the parameter string, omitting all the non-letters
-    static String returnString (String str) {
+   // static subroutine that finds the reverse of a string 
+   public static String revString( String str ) {
+        // declare and initalize empty string variable
+        String reverse = "";
+        // declare decrement variable 
+        int i;
+        // loop through user input string
+        for (i = str.length() - 1; i >= 0; i--) {
+        // add letters to variable in reverse order
+        reverse = reverse + str.charAt(i);
+        // return reversed string  }
         
-        String char = "";
-        str = str.toLowerCase();
-        if (char >= 'a' && char <= 'z');
-        return str
-    }
-    }
-}
-
-
-
-    
-    stripped = stripped.toLowerCase();
-    // print the string converted to lower case and stripped of any non-letter characters
-}
+      }
+       return reverse;
+   } // end of revString
+   
+   /** 
+   * static subroutine that makes a new string
+   * (copy of the parameter string with non-letters removed)
+   */ 
+   public static String stripString( String str ) {
+       // go through each character and check if it's a lower case letter
+         String stripped; // initialize variable
+         stripped = ""; // set variable equal to empty string
+         stripped = str.replaceAll("[^a-zA-Z]", "");          
+         return stripped; // return new string
+ } // end of stripString
+   
+  // reads in a string from the user and alerts user whether or not it is a palindrome 
+  public static void main(String[] args) {
+        //initalize and declare variables
+        String input_text;
+        String lower_string;
+        String stripped = "";
+        String reversed = "";
+        //prompt user for string input
+        TextIO.put("enter a string to see if it's a palindrome: ");
+        //store user input in a variable
+        input_text = TextIO.getln();
+        //System.out.println(input_text);
+        lower_string = input_text.toLowerCase();
+        //System.out.println(lower_string);
+        
+        // call strip subroutine, assigning value to "stripped"
+        stripped = stripString(lower_string);
+        System.out.println("stripped: " + stripped);
+        
+        // call revString using stripped as parameter
+        // assign value to "reversed"
+        reversed = revString(stripped);
+        System.out.println("reversed: " + reversed);
+        
+        // if block to provide feedback to user
+        if (!stripped.equals(reversed)){
+            // if reversed is not equal to stripped it is NOT a palindrome
+           System.out.println("This is NOT a palindrome.");
+       }else{
+           // otherwise, it is a palindrome
+           System.out.println("This IS a palindrome.");   
+       }
+   }     
+   } // end of firstsubroutines
