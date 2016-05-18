5cc13f8eadb8a99fc95a5fcf1109325fe126b0a8
Daniel Strong <danielstrong@mac.com>
2016-05-17 20:00:38 -0800

    add payroll system

--- PayrollSystem/src/payrollsystem/Employee.java
+++ PayrollSystem/src/payrollsystem/Employee.java
@@ -7,8 +7,31 @@ public abstract class Employee {
     private String name;
     private Vehicle vehicle;
 
+    public Employee() {
+        // default constructor is a constructor with zero parameters
+        System.out.println("...inside default constructor");
+        empId = 0;
+        name = "";
+    }
+    
+    public Employee (int pEmpId, String pName, Vehicle pV) {
+        // a non default constructor is a constructor with at least 1 parameter
+        System.out.println("...inside Employee non-default constructor");
+        empId = pEmpId;
+        name = pName;
+        this.vehicle = pV; // pV must have been previously created in the calling
+    }
+     
+    public Employee (int pEmpId, String pName, String pPlate, String pColor) {
+        // a non default constructor is a constructor with at least one parameter
+        System.out.println("... inside Employee non default constructor")
+        empId = pEmpId;
+        name = pName;
+        this.vehicle = new Vehicle(pPlate, pColor);
+    }
     
     public abstract double calculatePay(); 
+    
     // Getters and Setters
     
     /**
